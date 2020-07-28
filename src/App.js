2a971f0ad8387ea347167934be1be7bb9a54ca7c
patricia <patriciapavia236@gmail.com>
2020-07-28 12:31:24 +1000

    create profile for user

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { useReducer } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './layout/Home';
-import Landing from './layout/Landing';
-import { ThemeProvider } from '@material-ui/core/styles';
-import PostMelon from './ui/theme';
-import { Box } from '@material-ui/core';
-import { StateContext } from './config/GlobalState';
-import stateReducer from './config/stateReducer';
-import NavBar from './layout/NavBar';
-import Profile from './layout/Profile';
+import React, { useReducer } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./layout/Home";
+import Landing from "./layout/Landing";
+import { ThemeProvider } from "@material-ui/core/styles";
+import PostMelon from "./ui/theme";
+import { Box } from "@material-ui/core";
+import { StateContext } from "./config/GlobalState";
+import stateReducer from "./config/stateReducer";
+import NavBar from "./layout/NavBar";
+import Profile from "./layout/Profile";
+import MyProfile from "./layout/MyProfile";
 
 function App() {
   const initialState = { posts: [], token: null, userID: null };
@@ -22,20 +23,20 @@ function App() {
             <Switch>
               <Route
                 exact
-                path='/'
+                path="/"
                 render={(props) => {
                   return <Landing {...props} />;
                 }}
               />
-              <Route exact path='/profile/user/:id' component={Profile} />
+              <Route exact path="/profile/user/:id" component={Profile} />
               <Route
                 exact
-                path='/home'
+                path="/home"
                 render={(props) => {
                   return <Home {...props} />;
                 }}
               />
-              <Route exact path="/profile/user/:id" component={Profile} />
+              <Route exact path="/myprofile/" component={MyProfile} />
             </Switch>
           </Router>
         </StateContext.Provider>
--- src/layout/Profile.js
+++ src/layout/Profile.js
@@ -1,61 +1,61 @@
 import React, { useState, useEffect } from "react";
 import baseurl from "./../api/localAPI";
+import { useGlobalState } from "../config/GlobalState";
 import localAPI from "./../api/localAPI";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import ShowPost from "../components/posts/ShowPost";
 
 function Profile() {
-  const [profile, setProfile] = useState([]);
+  const { store, dispatch } = useGlobalState();
+  const { userID, posts } = store;
   const [userDetails, setUserDetails] = useState([]);
   const [userFriends, setUserFriends] = useState([]);
 
   let { id } = useParams();
+  const userPosts = posts.filter((post) => {
+    return post.authorId === id;
+  });
 
   useEffect(() => {
     try {
       (async () => {
-        const response = await localAPI.get(`profile/user/${id}`);
-        setProfile(response.data);
+        const response = await localAPI.get(`/users/${id}`);
+        setUserDetails(response.data);
+        setUserFriends(response.data.friends);
+        console.log(response.data.friends);
         console.log(response.data);
-
-        // // Get Post that belong to user this user
-        // const postByUserId = await localAPI.get(`/posts/${id}`);
-        // console.log(postByUserId);
-
-        // // get user details
-        const res = await localAPI.get(`/users/${id}`);
-        setUserDetails(res.data);
-        setUserFriends(res.data.friends);
-
-        console.log(res);
       })();
     } catch (e) {
       console.log(e);
     }
   }, []);
 
-  const friends = userFriends.map((friend) => {
-    return (
-      <div>
-        <ul>
-          <li>Friend Name: {friend.name}</li>
-        </ul>
-      </div>
-    );
-  });
-
-  return profile && userDetails ? (
+  return userDetails && userFriends ? (
     <div>
       <h1>this is profile</h1>
 
       <Link to={`/home/`}>Back to home</Link>
       <br></br>
       <button className="">Add Friend</button>
-      <h1>my bio{profile.bio}</h1>
-      <h1>my dob {profile.dob}</h1>
+
+      <h1>my bio: {userDetails.bio}</h1>
+      <h1>my dob: {userDetails.dob}</h1>
       <h1>Join Date: {userDetails.date}</h1>
 
-      {friends}
+      <div>
+        {userFriends.map((friend) => (
+          <ul>
+            <li>{friend.name}</li>
+          </ul>
+        ))}
+      </div>
+
+      <div>
+        {userPosts.map((post) => {
+          return <ShowPost post={post} />;
+        })}
+      </div>
     </div>
   ) : (
     <div>
