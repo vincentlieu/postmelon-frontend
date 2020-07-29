63a2061aab7e99cf1bca3121eaf748f8ec1056d5
vincentlieu <v.lieu93@gmail.com>
2020-07-29 23:21:47 +1000

    POS-53 - Added styling to Landing using material-UI

--- src/layout/Landing.js
+++ src/layout/Landing.js
@@ -1,39 +1,78 @@
-import React, { useState } from "react";
-import { Link } from 'react-router-dom'
-import Register from "../components/users/Register";
-import Login from "../components/users/Login";
-import MediaControlCard from "./MediaControlCard";
-import NewUser from "./NewUser"
-import Displayimage from "../assets/postmelon8.jpg"
+import React, { useState } from 'react';
+import Register from '../components/users/Register';
+import Login from '../components/users/Login';
+import {
+  Box,
+  Grid,
+  Paper,
+  Link,
+  Avatar,
+  Button
+} from '@material-ui/core';
+
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+}));
 
 const Landing = ({ history }) => {
-  const [renderRegister, setRenderRegister] = useState(true)
-  const [renderLogin, setRenderLogin] = useState(false)
+  const [renderRegister, setRenderRegister] = useState(false);
+  const [renderLogin, setRenderLogin] = useState(true);
+
+  const classes = useStyles();
 
   const handleRegister = () => {
-    setRenderRegister(true)
-    setRenderLogin(false)
-  }
+    setRenderRegister(true);
+    setRenderLogin(false);
+  };
 
   const handleLogin = () => {
-    setRenderRegister(false)
-    setRenderLogin(true)
-  }
+    setRenderRegister(false);
+    setRenderLogin(true);
+  };
 
   return (
-    <center>
-      <div className='login-container'>
-        <img src={Displayimage} className='landing-image'/>
-        <div className="landing-register-login">  
-          <div className="pipes"> 
-            <span onClick={handleRegister} className="registerpipe">Register|</span> 
-            <span onClick={handleLogin} className="loginpipe">Login</span>
-          </div>
-            {renderRegister && <Register /> || renderLogin && <Login history={history} />} 
-        </div>
-      </div>
-    </center>
-
+    <Grid container component='main' className={classes.root}>
+      <Grid item xs={false} sm={4} md={7} className='landing-image' />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <Box className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Box my={2}>
+            <Button
+              onClick={handleLogin}
+              className='loginpipe'
+              color={renderLogin ? 'primary' : 'inherit'}>
+              Login
+            </Button>
+            <Button
+              onClick={handleRegister}
+              className='registerpipe'
+              color={renderRegister ? 'primary' : 'inherit'}>
+              Register
+            </Button>
+          </Box>
+          {(renderRegister && <Register />) ||
+            (renderLogin && <Login history={history} />)}
+        </Box>
+      </Grid>
+    </Grid>
   );
 };
 
